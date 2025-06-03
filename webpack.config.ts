041500a86e36bb6648c354304e7e7f88fe6ee65f
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2025-06-03 17:31:51 +0300

    chore: add TypeScript and basic tsconfig

---
+++ webpack.config.ts
@@ -0,0 +1,68 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import "webpack-dev-server";
+
+type Mode = "production" | "development";
+
+interface EnvVariables {
+  mode: Mode;
+}
+
+export default (env: EnvVariables): webpack.Configuration => {
+  return {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+  };
+};
+
+// const config: webpack.Configuration = (env: any) => {
+//     mode: env.mode ?? "development",
+//     entry: path.resolve(__dirname, "src", "index.ts"),
+//     output: {
+//       path: path.resolve(__dirname, "build"),
+//       filename: "[name].[contenthash].js",
+//       clean: true,
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: path.resolve(__dirname, "public", "index.html"),
+//       }),
+//     ],
+//     module: {
+//       rules: [
+//         {
+//           test: /\.tsx?$/,
+//           use: "ts-loader",
+//           exclude: /node_modules/,
+//         },
+//       ],
+//     },
+//     resolve: {
+//       extensions: [".tsx", ".ts", ".js"],
+//     },
+// };
+
+// export default config;
